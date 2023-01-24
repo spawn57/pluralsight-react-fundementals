03e97042faddfe3927ac4bdf60495e336d803bfc
Sunil Khiatani <skhiatani@gmail.com>
2023-01-24 20:14:44 +0800

    added conditional formatting

--- components/houseRow.js
+++ components/houseRow.js
@@ -6,7 +6,9 @@ const HouseRow = ({house}) => {
             <tr>
                 <td>{house.address}</td>
                 <td>{house.country}</td>
-                <td>{currencyFormatter.format(house.price)}</td>
+                <td className={`${house.price >= 500000 ? "text-primary" : ""}`}>
+                    {currencyFormatter.format(house.price)}
+                </td>
             </tr>
         </>
     )
